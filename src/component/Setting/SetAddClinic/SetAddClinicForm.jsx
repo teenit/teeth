15024431d0b686c93fd4eb16eba86fcf247a8c76
Pavlo <teenitclub@gmail.com>
2023-06-17 11:36:33 +0300

    Add-clinics

--- src/component/Setting/SetAddClinic/SetAddClinicForm.jsx
+++ src/component/Setting/SetAddClinic/SetAddClinicForm.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import s from './style.module.css';
 import CloseBtn from "../../Modules/CloseBtn/CloseBtn";
 import { api } from "../../../functions/api";
+import { Grid, TextField } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { TimeField } from "@mui/x-date-pickers";
 
 const SetAddClinicForm = ({close})=>{
 
-    const [state,setState] = useState({clinic:{name:"",startTime:"",endTime:""}})
-
+    const [state,setState] = useState({clinic:{name:"",startTime:"",endTime:"",address:""}})
+    const sx = {
+        width:"100%"
+    }
     const saveClinic = (e)=>{
         e.preventDefault()
         api(state,'manage/add-clinic.php')
@@ -17,21 +22,55 @@ const SetAddClinicForm = ({close})=>{
             <CloseBtn close={close}/>
             <h3>Додати клініку</h3>
             <form className={s.form} action="" onSubmit={saveClinic}>
-                <div>
-                    <label htmlFor="">Назва клініки</label>
-                    <input value={state.clinic.name} type="text" required placeholder="Назва клініки" onChange={(e)=>{
-                        setState({...state,clinic:{...state.clinic,name:e.target.value}})
-                    }}/>
+                <div>       
+                    <TextField value={state.clinic.name} 
+                                type="text" 
+                                required 
+                                variant="standard" 
+                                sx={sx}
+                                label="Назва клініки"
+                                onChange={(e)=>{
+                                    setState({...state,clinic:{...state.clinic,name:e.target.value}})
+                                }}/>
+                </div>
+                <div>       
+                    <TextField value={state.clinic.address} 
+                                type="text" 
+                                required 
+                                sx={sx}
+                                variant="standard" 
+                                label="Адреса клініки"
+                                onChange={(e)=>{
+                                    setState({...state,clinic:{...state.clinic,address:e.target.value}})
+                                }}/>
                 </div>
                 <div>
-                    <label htmlFor="">Час роботи</label>
-
-                    <input value={state.clinic.startTime} type="time" required onChange={(e)=>{
-                        setState({...state,clinic:{...state.clinic,startTime:e.target.value}})
-                    }}/>
-                    <input value={state.clinic.endTime} type="time" required  onChange={(e)=>{
-                        setState({...state,clinic:{...state.clinic,endTime:e.target.value}})
-                    }}/>
+                    <p>Час роботи клініки</p>
+                <Grid container spacing={2}>
+                    <Grid>
+                    <TextField value={state.clinic.startTime} 
+                                type="time" 
+                                required 
+                                
+                                variant="standard" 
+                                label="Початок"
+                                onChange={(e)=>{
+                                    setState({...state,clinic:{...state.clinic,startTime:e.target.value}})
+                                }}/>
+                    </Grid>
+                    <Grid>
+                       
+                    <TextField value={state.clinic.endTime} 
+                                type="time" 
+                                required 
+                                
+                                variant="standard" 
+                                label="Кінець"
+                                onChange={(e)=>{
+                                    setState({...state,clinic:{...state.clinic,endTime:e.target.value}})
+                                }}/>
+                </Grid>
+                </Grid>
                 </div>
                 <div>
                     <button className="btn">Додати клініку</button>
