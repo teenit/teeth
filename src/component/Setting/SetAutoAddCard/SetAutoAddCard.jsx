5e9091adb0b2c9b06bd0a2f79279819ec0ec9f81
kleema <vovanklim2006@gmail.com>
2023-06-17 13:01:07 +0300

    checkbox function setting number

--- src/component/Setting/SetAutoAddCard/SetAutoAddCard.jsx
+++ src/component/Setting/SetAutoAddCard/SetAutoAddCard.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import s from "./style.module.css"
+import { CheckBox } from "@mui/icons-material";
+import { Button, TextField } from "@mui/material";
 const SetAutoAddCard= ()=>{
+    const [active,setActive]=useState(true)
 return (
-    <div>SetAutoAddCard</div>
+    <div className={s.wrap}>
+        <p>встановити автоматичне присвоєння номерів карток</p>
+        <input type="checkbox"  className={s.input} onChange={()=>{
+            setActive(!active)
+        }}/>
+        <p>введіть номер останньої картки</p>
+        <TextField disabled={active}
+  hiddenLabel
+  type="number"
+  id="filled-hidden-label-small"
+  variant="filled"
+  size="small"
+/>
+<div><Button variant="contained" className={s.button}   onClick={console.log()}>Зберегти</Button></div>
+    </div>
+    
+
 )
     
 
