3e423243da42f0dedfa7fb003387ef0513f32672
kleema <vovanklim2006@gmail.com>
2023-06-17 11:36:57 +0300

    AutoAdd-card

--- src/component/Setting/SetAutoAddCard/SetAutoAddCard.jsx
+++ src/component/Setting/SetAutoAddCard/SetAutoAddCard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-
+import s from "./style.module.css"
+import { CheckBox } from "@mui/icons-material";
+import { Button, TextField } from "@mui/material";
 const SetAutoAddCard= ()=>{
 return (
-    <div>SetAutoAddCard</div>
+    <div className={s.wrap}>
+        <p>встановити автоматичне присвоєння номерів карток</p>
+        <input type="checkbox"  className={s.input}/>
+        <p>введіть номер останньої картки</p>
+        <TextField
+  hiddenLabel
+  type="number"
+  id="filled-hidden-label-small"
+  variant="filled"
+  size="small"
+/>
+<div><Button variant="contained" className={s.button}>Зберегти</Button></div>
+
+    </div>
 )
     
 
