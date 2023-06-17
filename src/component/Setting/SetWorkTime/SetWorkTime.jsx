6d1f9d5c7cb7ede2f584ae385c836aaaeb1a8176
antimightbee <feedbacktoanti@gmail.com>
2023-06-17 11:36:28 +0300

    set-work-time

--- src/component/Setting/SetWorkTime/SetWorkTime.jsx
+++ src/component/Setting/SetWorkTime/SetWorkTime.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-import s from './style.module.css'
+import React, { useState } from 'react'
+import s from './SetWorkTime.module.css'
+import WorkTimeRow from './WorkTimeRow'
+import { Button } from '@mui/material'
 
 const SetWorkTime = () => {
+
+  const workTime = [
+    {
+      title:"Перша",
+    },
+    {
+      title:"Друга",
+    },
+    {
+      title:"Повна",
+    },
+  ]
+  const addedData = {title:"Нова зміна"}
+  const [work, setWork] = useState([...workTime])
+  const addData = () => {
+    const newData = [...work]
+    newData.push(addedData)
+    setWork(newData)
+  };
   return (
-    <div>SetWorkTime</div>
+    <div className={s.wrap}>
+      <h3>Робочі зміни</h3>
+            <WorkTimeRow work = {work} setWork = {(arg)=>{
+              setWork(arg)
+            }}/>
+      <div className={s.add__row} onClick={()=>{
+          addData()
+      }}>
+        <div className={s.r1}></div>
+        <div className={s.r2}></div>
+      </div>
+      <Button variant="contained" onClick={()=>{
+        if(work.length==0){
+          alert("Додайте хоча б одну зміну")
+        }
+        console.log(work);
+      }}>Зберегти</Button>
+    </div>
   )
 }
 
